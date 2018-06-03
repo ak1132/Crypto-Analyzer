39898a47989d3fcd4fcedaa6a6414e5faf69d7e8
Amogh Kulkarni <amogh.kulkarni1993@gmail.com>
2018-06-03 13:39:00 -0400

    moved notebook files and modularizing the py files

---
+++ coinmarketcal.py
@@ -0,0 +1,75 @@
+import logging
+import requests
+import pandas as pd
+
+client_id = "283_3vv0ugxcf8kkggk0goowww8sckwck4kgsw8okg8cw0wg44o40g"
+client_secret = "5vq8b6o6qxs08goc80oocccokscgo0ow0o4swk8wkg8o0s4kc"
+grant_type = "client_credentials"
+coins_list_url = "https://api.coinmarketcal.com/v1/coins?"
+access_token_url = "https://api.coinmarketcal.com/oauth/v2/token?"
+events_list_url = "https://api.coinmarketcal.com/v1/events?"
+
+
+def query_coinmarketcal_get_access_token(grant, client, secret):
+    """ Query CoinMarketCal API """
+    url = access_token_url + "grant_type=" + grant + "&client_id=" + client + "&client_secret=" + secret
+    try:
+        response = requests.get(url).json()
+    except Exception as e:
+        logging.error("Failure while querying {query}. \n{err}".format(query=url, err=e))
+        return None
+
+    if not response:  # or 'Response' not in response.keys():
+        return response
+    return response
+
+
+access_token = query_coinmarketcal_get_access_token(grant_type, client_id, client_secret)['access_token']
+
+
+def query_coinmarketcal_get_coins_list(access_token):
+    """ Query CoinMarketCal API to get Coins List"""
+    coins_url = coins_list_url + "access_token=" + access_token
+    try:
+        response = requests.get(coins_url).json()
+    except Exception as e:
+        logging.error("Failure while querying {query}. \n{err}".format(query=coins_url, err=e))
+        return None
+
+    if not response:  # or 'Response' not in response.keys():
+        return response
+    return response
+
+
+df = pd.DataFrame(query_coinmarketcal_get_coins_list(
+    query_coinmarketcal_get_access_token(grant_type, client_id, client_secret)['access_token']))
+df.to_csv("coins.csv")
+print("Done")
+
+
+def query_coinmarketcal_get_events_list(access_token):
+    """ Query CoinMarketCal API to get Events List"""
+    loop = True
+    i = 1
+    final_response = []
+    while (loop):
+        events_url = events_list_url + "access_token=" + access_token + "&page=" + str(i)
+        try:
+            response = requests.get(events_url).json()
+            if 'code' in response and response.code == 404:
+                loop = False
+                break
+            i = i + 1
+            for item in response:
+                final_response.append(response)
+        except Exception as e:
+            logging.error("Failure while querying {query}. \n{err}".format(query=events_url, err=e))
+            loop = False
+    return final_response
+
+
+json_response = query_coinmarketcal_get_events_list(
+    query_coinmarketcal_get_access_token(grant_type, client_id, client_secret)['access_token'])
+df = pd.DataFrame(json_response)
+df.to_csv("Events.csv")
+print("Done")
---
+++ coinmarketcap.py
@@ -0,0 +1,51 @@
+
+# coding: utf-8
+
+# In[2]:
+
+
+import json 
+import requests 
+import pandas as pd
+import numpy as np
+import sys
+import collections
+
+r = requests.get('https://api.coinmarketcap.com/v2/listings/')
+#print(r.json()['data'])
+length = len(r.json()['data'])
+print(length)
+res = []
+j=0
+coins_list = []
+for i in range(1,length+1,100):
+    r = requests.get('https://api.coinmarketcap.com/v2/ticker/?convert=BTC&start='+str(i))
+    for attribute,value in r.json().items():
+        #print(attribute,value)
+        
+        for id,val in value.items():
+            #print(id,val)
+            coin = {}
+            #print(val)
+            if type(val) is dict:
+                for key in val:
+                    if key == "quotes": 
+                        market_cap_USD = val[key]['USD']['market_cap']
+                        coin['market_cap_USD'] = market_cap_USD
+                        market_cap_BTC = val[key]['BTC']['market_cap']
+                        coin['market_cap_BTC'] = market_cap_BTC
+                        price_BTC = val[key]['BTC']['price']
+                        coin['price_BTC'] = price_BTC
+                        volume_24hr_BTC = val[key]['BTC']['volume_24h']
+                        coin['volume_24hour_BTC'] = volume_24hr_BTC
+                        volume_24hr_USD = val[key]['USD']['volume_24h']
+                        coin['volume_24hour_USD'] = volume_24hr_USD
+                        continue
+                    coin[key] = val[key]
+            j =j+1
+            coins_list.append(coin)
+#print(coins_list)
+dataframe = pd.DataFrame(coins_list)
+dataframe.to_csv('coinmarketcap.csv')
+print("Done")
+
